a101f2c9be06f8429dada7e408f5a34522a90068
eduardo moreira T <38986601+eduardoromeo@users.noreply.github.com>
2024-04-16 22:24:29 -0400

    Update Main.java
    fullcode

--- src/main/java/com/livethinking/Main.java
+++ src/main/java/com/livethinking/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
- 
+ //By: Eduardo Moreira Tellez
 public class Main {
   public static void main(String[] args) {
 
